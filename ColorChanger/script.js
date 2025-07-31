353ff89aa06fa3c8539f770f7ed1163aab0b2e58
Mayank Chauhan <124248806+Mayankk499@users.noreply.github.com>
2025-07-31 23:08:47 +0530

    script.js

--- ColorChanger/script.js
+++ ColorChanger/script.js
@@ -2,22 +2,39 @@ const btn = document.querySelectorAll('.button')
 const bdy = document.querySelector("body")
 
 btn.forEach(function(button){
-    console.log(button)
     button.addEventListener('click', function(e){
-        console.log(e)
-        console.log(e.target)
-        if(e.target.id === 'grey'){
-            bdy.style.backgroundColor = e.target.id;
-        }
-        if(e.target.id === 'red'){
-            bdy.style.backgroundColor = e.target.id;
-        }
-        if(e.target.id === 'orange'){
-            bdy.style.backgroundColor = e.target.id;
-        }
-        if(e.target.id === 'blue'){
-            bdy.style.backgroundColor = e.target.id;
-        }
+
+        // IF Statement
+        
+        // if(e.target.id === 'grey'){
+        //     bdy.style.backgroundColor = e.target.id;
+        // }
+        // if(e.target.id === 'red'){
+        //     bdy.style.backgroundColor = e.target.id;
+        // }
+        // if(e.target.id === 'orange'){
+        //     bdy.style.backgroundColor = e.target.id;
+        // }
+        // if(e.target.id === 'blue'){
+        //     bdy.style.backgroundColor = e.target.id;
+        // }
+
+        // SWITCH Statement
+
+        switch (e.target.id) {
+            case 'grey':
+                body.style.backgroundColor = e.target.id
+                break;
+            case 'red':
+                body.style.backgroundColor = e.target.id
+                break;
+            case 'orange':
+                body.style.backgroundColor = e.target.id
+                break;
+            case 'blue':
+        
+            default:
+                break;
         
     })
 });
